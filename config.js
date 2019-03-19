3f095b6ae93d5813c5d8ca6fa5a9b0723f29259e
Alexander Quach <alexquac@gmail.com>
2019-03-18 23:48:55 -0700

    updated users testing

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/plants';
-// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://testUser:testPassword@plantcare-api-zsrlc.mongodb.net/Plants?retryWrites=true';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-plants-db';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
--- test/test-users.js
+++ test/test-users.js
@@ -10,9 +10,9 @@ const {TEST_DATABASE_URL} = require('../config');
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-// function teardownDb() {
-//   return mongoose.connection.dropDatabase();
-// }
+function teardownDb() {
+  return mongoose.connection.dropDatabase();
+}
 
 describe('Users', function() {
   before(function() {
@@ -22,10 +22,11 @@ describe('Users', function() {
   //   return teardownDb();
   // });
   after(function() {
+    teardownDb();
     return closeServer();
   });
 
-  const testUser = {username: 'testUsername', password: 'testPassword'}
+  const testUser = {username: 'username', password: 'testPassword'}
 
   it('should post a new user', function() {
     return chai.request(app)
@@ -44,27 +45,26 @@ describe('Users', function() {
     .then(function(res) {
       expect(res).to.have.status(200);
       expect(res).to.be.json;
-      expect(res.body).to.be.a('object');
-      expect(res.body).to.have.a.length.of.at.least(1);
-      expect(res.body).to.include.keys('username');
+      expect(res.body).to.be.a('array');
+      expect(res.body).to.have.length.of.at.least(1);
     });
   });
 
   it('should find a specifc user', function() {
     return chai.request(app)
-    .get('/users/testUsername')
+    .get(`/users/${testUser.username}`)
     .then(function(res) {
       expect(res).to.have.status(200);
       expect(res).to.be.json;
       expect(res.body).to.be.a('object');
-      expect(res.body).to.have.a.length.of.at.least(1);
+      expect(res.body).to.include.keys('username');
     });
   });
 
   const testMessage = {
-    username: 'testUsername',
+    username: `${testUser.username}`,
     message: 'test message',
-    recipient: 'testUsername'
+    recipient: `${testUser.username}`
   }
 
   it('should post a message', function() {
@@ -81,7 +81,7 @@ describe('Users', function() {
 
   it('should find user messages', function() {
     return chai.request(app)
-    .get('/users/messages/testUsername')
+    .get(`/users/messages/${testUser.username}`)
     .then(function(res) {
       expect(res).to.have.status(200);
       expect(res).to.be.json;
